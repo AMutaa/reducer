2ecd4acb75c9b2e58515afac022456d38e8e0c27
AMutaa <piusandru@gmail.com>
2018-10-05 15:49:31 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
-import {createStore} from 'redux'
+import './App.css';
+import Counter from './Counter'
 
 class App extends Component{
   render(){
     return(
-      <div>
-        <h1>Hello</h1>
+      <div className="counter">
+        <Counter/>
       </div>
     )
   }
@@ -13,22 +14,3 @@ class App extends Component{
 
 export default App;
 
-const defaultState = {
-  welcome: 'Hi',
-  otherState: 'some stuff'
-}
-
-const greeting = (state= defaultState, action) => {
- switch(action.type){
-  case 'GREET_ME': 
-    return{ welcome: 'Hello Scott'}
-  case 'GREET_WORLD':
-    return { welcome: 'Hello Adam'}
-  default:
-  return state
- }
-};
-
-
-const store = createStore(greeting);
-console.log(store.getState());
---
+++ src/Counter.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+
+function Counter(props) {
+  return(
+    <div>
+      <h1>Counter</h1>
+    </div>
+  )
+}
+
+export default Counter;
