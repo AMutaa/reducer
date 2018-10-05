26600a9b473820a2a0bb55546a7e2bc0d23a410d
AMutaa <piusandru@gmail.com>
2018-10-05 16:29:26 -0400

    closing the reducer

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,12 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
     console.log('reducer running', action);
-    return state
+    switch(action.type){
+        case 'INCREMENT':
+         return Object.assign({}, state, { count: state.count + 1})
+        default:  
+         return state
+    }
 }
 
 const store = createStore(reducer);
